9d607a25b8309b10b62f287e53204e9c35a1d8bc
zgh123 <362115629@qq.com>
2021-03-19 18:21:48 +0800

    打打
    阿第三十

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
