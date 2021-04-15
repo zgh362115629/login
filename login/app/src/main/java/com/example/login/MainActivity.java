aba42b34ff493b8fdc3f64a5776a6721af3b8c9a
zgh123 <362115629@qq.com>
2021-04-15 22:35:07 +0800

    0.1
    0213

--- login/app/src/main/java/com/example/login/MainActivity.java
+++ login/app/src/main/java/com/example/login/MainActivity.java
@@ -5,18 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button_login);
+        setContentView(R.layout.module_activity);
+        Button button = (Button) findViewById(R.id.btn_login);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                EditText editText1 = findViewById(R.id.et_user);
+                EditText editText2 = findViewById(R.id.et_pw);
+                String user = editText1.getText().toString();
+                String pw = editText2.getText().toString();
+                intent.putExtra("user",user);
+                intent.putExtra("pw",pw);
                 startActivity(intent);
             }
         });
--- login/app/src/main/java/com/example/login/MainActivity2.java
+++ login/app/src/main/java/com/example/login/MainActivity2.java
@@ -1,13 +1,137 @@
 package com.example.login;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private List<Fruit> fruitList = new ArrayList<Fruit>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.module_activity2);
+
+        Intent intent = getIntent();
+        String user = intent.getStringExtra("user");
+        String pw = intent.getStringExtra("pw");
+        TextView textView1 = findViewById(R.id.tv_showUser);
+        TextView textView2 = findViewById(R.id.tv_showpw);
+        textView1.setText(user);
+        textView2.setText(pw);
+        initFruits(); // 初始化水果数据
+        FruitAdapter adapter = new FruitAdapter(MainActivity2.this, R.layout.item, fruitList);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity2.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    private void initFruits() {
+        for (int i = 0; i < 2; i++) {
+            Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+            fruitList.add(apple);
+            Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+            fruitList.add(banana);
+            Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+            fruitList.add(orange);
+            Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+            fruitList.add(watermelon);
+            Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+            fruitList.add(pear);
+            Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+            fruitList.add(grape);
+            Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+            fruitList.add(pineapple);
+            Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+            fruitList.add(strawberry);
+            Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
+            fruitList.add(cherry);
+            Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+            fruitList.add(mango);
+        }
+    }
+
 }
+
+class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
+
+class FruitAdapter extends ArrayAdapter<Fruit> {
+
+    private int resourceId;
+
+    public FruitAdapter(Context context, int textViewResourceId,
+                        List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position); // 获取当前项的Fruit实例
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById (R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById (R.id.fruit_name);
+            view.setTag(viewHolder); // 将ViewHolder存储在View中
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag(); // 重新获取ViewHolder
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
+        return view;
+    }
+
+    class ViewHolder {
+
+        ImageView fruitImage;
+
+        TextView fruitName;
+
+    }
+
+}
+
